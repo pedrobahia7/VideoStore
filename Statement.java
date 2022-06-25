59946676d6f4dc2ada653d632730219c3bb72010
pedrobahia7 <pedrobahia7@hotmail.com>
2022-06-25 15:56:35 -0300

    Commit 18

--- Statement.java
+++ Statement.java
@@ -1,3 +1,21 @@
-public class Statement {
+import java.util.Enumeration;
+public abstract class Statement {
+
+    public String value(Customer aCustomer) {
+        Enumeration rentals = aCustomer.getRentals();
+        String result = getHeader(aCustomer);
+        while (rentals.hasMoreElements()) {
+           Rental each = (Rental) rentals.nextElement();
+           //show figures for each rental
+           result += getRentalFigures(each);
+        }
+        //add footer lines
+        result += getFooter(aCustomer);
+        return result;
+    }
+
+    public abstract String getHeader(Customer aCustomer);
+    public abstract String getRentalFigures(Rental aRental);
+    public abstract String getFooter(Customer aCustomer);
 
 }
--- TextStatement.java
+++ TextStatement.java
@@ -1,19 +1,4 @@
-import java.util.Enumeration;
-
 public class TextStatement extends Statement {
-
-    public String value(Customer aCustomer) {
-        Enumeration rentals = aCustomer.getRentals();
-        String result = getHeader(aCustomer);
-        while (rentals.hasMoreElements()) {
-           Rental each = (Rental) rentals.nextElement();
-           //show figures for this rental
-           result += getRentalFigures(each);
-        }
-        //add footer lines
-        result += getFooter(aCustomer);
-        return result;
-    }
 
     public String getHeader(Customer aCustomer) {
         return "Rental Record for " + aCustomer.getName() +
